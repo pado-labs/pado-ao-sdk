1f3ca345a0d579d1c594c2730b5b2538d853d0dc
fksyuan <fksyuan@163.com>
2024-05-07 14:04:52 +0800

    update uploadData interface

--- src/index.ts
+++ src/index.ts
@@ -7,26 +7,32 @@ import { submitDataToAR, getDataFromAR } from "./padoarweave";
 import Arweave from 'arweave';
 import { readFileSync } from "node:fs";
 
+export interface PriceInfo {
+  price: string;
+  symbol?: string;
+}
+
 /**
  * Encrypt data and upload data
  *
  * @param data - plain data need to encrypt and upload
  * @param dataTag - the data meta info
- * @param signer - The ar wallet signer
- * @param price - The data price using PADO Token
+ * @param priceInfo - The data price symbol and price
+ * @param wallet - The ar wallet
+ * @param arweave - The ar object and default is ar production
  * @returns The uploaded encrypted data id
  *
  */
-export const uploadData = async (data: Uint8Array, dataTag: string, wallet: any, price: string, arweave: Arweave) => {
-    // 1. get pado node public key
-    // 2. invoke algorithm encrypt
-    // 3. upload encrypted data to AR
-    // 4. register encrypted keys and ar data url to ao data process
-    // 5. return data id
+export const uploadData = async (data: Uint8Array, dataTag: any, priceInfo: PriceInfo,
+  wallet: any, arweave: Arweave = Arweave.init({})): Promise<string> => {
+    if (data.length === 0) {
+      throw new Error("The Data to be uploaded can not be empty");
+    }
+
+    priceInfo.symbol = priceInfo.symbol || "PADO Token";
 
     let nodesres = await nodes();
     nodesres = JSON.parse(nodesres);
-
     let nodepks =Object();
     for(let i in nodesres) {
         let node = nodesres[i];
@@ -37,21 +43,20 @@ export const uploadData = async (data: Uint8Array, dataTag: string, wallet: any,
         nodepks["testnode2"],
         nodepks["testnode3"],
     ];
+
     const res = encrypt(nodesPublicKey, data);
-    //console.log("encrypt res=", res);
 
     const transactionId = await submitDataToAR(arweave, res.enc_msg, wallet);
-    console.log("uploadData ar transactionId=", transactionId);
-    console.log("uploadData ar enc_msg=", res.enc_msg);
 
     const signer = createDataItemSigner(wallet);
     const encSksStr = JSON.stringify(res.enc_sks);
-    const dataRes = await dataRegister(dataTag,
-      price, encSksStr, res.nonce, transactionId, signer);
+    const dataRes = await dataRegister(JSON.stringify(dataTag),
+      JSON.stringify(priceInfo), encSksStr, res.nonce, transactionId, signer);
+
     return dataRes;
 }
 
-export const submitTask = async (dataId: string, dataUserPk: string, wallet: any) => {
+export const submitTask = async (dataId: string, dataUserPk: string, wallet: any): Promise<string> => {
   const signer = createDataItemSigner(wallet);
   let inputData = {...THRESHOLD_2_3, dataId: dataId, consumerPk: dataUserPk};
   const taskId = await submit("ZKLHEDataSharing", JSON.stringify(inputData),
@@ -59,10 +64,8 @@ export const submitTask = async (dataId: string, dataUserPk: string, wallet: any
   return taskId;
 }
 
-export const getResult = async (taskId: string, dataUserSk: string, arweave: Arweave) => {
-  // 1. get encrypted result
-  // 2. invoke algorithm get plain data
-
+export const getResult = async (taskId: string, dataUserSk: string,
+  arweave: Arweave = Arweave.init({})) => {
   const taskStr = await getCompletedTasksById(taskId);
   const task = JSON.parse(taskStr);
   if (!task.id) {
@@ -98,7 +101,7 @@ export const SubmitTaskAndGetResult = async (dataId: string, publicKey: string) 
 
 async function test() {
     const wallet = JSON.parse(
-        readFileSync("./arweave-keyfile-JNqOSFDeSAh_icEDVAVa_r9wJfGU9AYCAJUQb2ss7T8.json").toString(),
+        readFileSync("/Users/fksyuan/Downloads/arweave-keyfile-JNqOSFDeSAh_icEDVAVa_r9wJfGU9AYCAJUQb2ss7T8.json").toString(),
     );
     //const signer = createDataItemSigner(wallet);
     const arweave = Arweave.init({
@@ -107,10 +110,10 @@ async function test() {
       protocol: 'http'
     });
 
-    const dataId = await uploadData(new Uint8Array([1,2,3,4,5,6]), "test", wallet, "1", arweave);
+    const dataId = await uploadData(new Uint8Array([1,2,3,4,5,6]), {"testtagkey": "testtagvalue"}, {price: "1"}, wallet, arweave);
     console.log("dataId=", dataId);
-    //const allDataRes = await allData();
-    //console.log("allDataRes=", allDataRes);
+    const dataResRes = await getDataById(dataId);
+    console.log("dataResRes=", dataResRes);
 
     const dataUserKey = keygen();
     //console.log("dataUserKey=", dataUserKey);
