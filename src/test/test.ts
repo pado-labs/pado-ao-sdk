be47f1b90f3f18f3c6593f592eac82809d15d694
fksyuan <fksyuan@163.com>
2024-05-16 16:40:09 +0800

    add the addWhiteList interface of node registry

---
+++ src/test/test.ts
@@ -0,0 +1,13 @@
+import { addWhiteList } from "../processes/noderegistry"
+import { readFileSync } from "node:fs";
+import { createDataItemSigner } from "@permaweb/aoconnect";
+
+const addWhiteNodeOwner = async (addr: string) => {
+    const wallet = JSON.parse(readFileSync("/Users/fksyuan/.aos.json").toString());
+    const signer = createDataItemSigner(wallet);
+    const res = await addWhiteList(addr, signer);
+    console.log("addWhiteNodeOwner res=", res);
+}
+
+addWhiteNodeOwner("XLmW13GJTV5Xn7K3jyNZQ4Dg5yHnfbzwtDmdE3DB92s");
+addWhiteNodeOwner("raZlGBd-pj2n0svwMOIV3WZePDVvTEV-PCYHJKlSrQ8");
